3c5ba5c7f888abfb7f791bbf3dab976d7d043c4a
John R Barker <john@johnrbarker.com>
2019-12-21 16:06:55 +0000

    Example filename

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -13,6 +13,7 @@ if find tests/output/coverage/ -mindepth 1 -name '.*' -prune -o -print -quit | g
     ansible-test coverage xml -v --requirements --group-by command --group-by version ${stub:+"$stub"}
 
     # upload coverage report to codecov.io
+    # Example: tests/output/coverage/integration=grafana_team=docker-default=python-3.6=coverage.98a48352c8fc.4207.186313
     for file in tests/output/coverage/coverage=*.xml; do
         flags="${file##*/coverage=}"
         flags="${flags%.xml}"
