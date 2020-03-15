f5245091888726e2da91074f1d7f1eb3e6386845
Rémi REY <rrey94@gmail.com>
2020-03-15 19:01:05 +0100

    Port ansible-collection-migration diff
    Closes: #66

--- plugins/lookup/grafana_dashboard.py
+++ plugins/lookup/grafana_dashboard.py
@@ -7,46 +7,45 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
 
-DOCUMENTATION = '''lookup: grafana_dashboard
+DOCUMENTATION = '''
+lookup: grafana_dashboard
 author: Thierry Salle (@seuf)
 short_description: list or search grafana dashboards
 description:
-- This lookup returns a list of grafana dashboards with possibility to filter them
-  by query.
+  - This lookup returns a list of grafana dashboards with possibility to filter them by query.
 options:
   grafana_url:
     description: url of grafana.
     env:
-    - name: GRAFANA_URL
+      - name: GRAFANA_URL
     default: http://127.0.0.1:3000
   grafana_api_key:
     description:
-    - api key of grafana.
-    - when C(grafana_api_key) is set, the options C(grafan_user), C(grafana_password)
-      and C(grafana_org_id) are ignored.
-    - Attention, please remove the two == at the end of the grafana_api_key
-    - because ansible lookup plugins options are split on = (see example).
+      - api key of grafana.
+      - when C(grafana_api_key) is set, the options C(grafan_user), C(grafana_password) and C(grafana_org_id) are ignored.
+      - Attention, please remove the two == at the end of the grafana_api_key
+      - because ansible lookup plugins options are split on = (see example).
     env:
-    - name: GRAFANA_API_KEY
+      - name: GRAFANA_API_KEY
   grafana_user:
     description: grafana authentication user.
     env:
-    - name: GRAFANA_USER
+      - name: GRAFANA_USER
     default: admin
   grafana_password:
-    description: grafana authentication password.
+    description:  grafana authentication password.
     env:
-    - name: GRAFANA_PASSWORD
+      - name: GRAFANA_PASSWORD
     default: admin
   grafana_org_id:
     description: grafana organisation id.
     env:
-    - name: GRAFANA_ORG_ID
+      - name: GRAFANA_ORG_ID
     default: 1
   search:
     description: optional filter for dashboard search.
     env:
-    - name: GRAFANA_DASHBOARD_SEARCH
+      - name: GRAFANA_DASHBOARD_SEARCH
 '''
 
 EXAMPLES = """
