083f0b7bc0a934d5177fd4bd48815de0afc357fb
Rémi REY <rrey94@gmail.com>
2022-10-06 22:04:48 +0200

    fix(find_grafana_versions): get more releases to have last 3 major versions

--- hacking/find_grafana_versions.py
+++ hacking/find_grafana_versions.py
@@ -11,7 +11,7 @@ def get_by_major(version):
 
 
 def get_grafana_releases():
-    r = requests.get('https://api.github.com/repos/grafana/grafana/releases', headers={"Accept": "application/vnd.github.v3+json"})
+    r = requests.get('https://api.github.com/repos/grafana/grafana/releases?per_page=50', headers={"Accept": "application/vnd.github.v3+json"})
     if r.status_code != 200:
         raise Exception("Failed to get releases from GitHub")
     return r.json()
