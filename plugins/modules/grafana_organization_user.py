f575413236e387228079e5f3cb8fce1b5bf429e7
Nemental <15136847+Nemental@users.noreply.github.com>
2023-11-03 17:00:07 +0100

    fix: get orga users by login

--- plugins/modules/grafana_organization_user.py
+++ plugins/modules/grafana_organization_user.py
@@ -190,7 +190,7 @@ class GrafanaOrganizationUserInterface(object):
 
     def _organization_user_by_login(self, org_id, login):
         for user in self._organization_users(org_id):
-            if user['name'] == login or user['email'] == login:
+            if login in (user['login'], user['email']):
                 return user
 
     def create_or_update_user(self, org_id, login, role):
