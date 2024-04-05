955ad32566cec508b8f8dabe6a79fa3aac0fb697
Nemental <15136847+Nemental@users.noreply.github.com>
2024-04-05 11:17:16 +0200

    fix: find grafana releases for latest 3 major versions

--- hacking/find_grafana_versions.py
+++ hacking/find_grafana_versions.py
@@ -7,12 +7,12 @@ import requests
 def get_by_major(version):
     if version.startswith("v"):
         version = version[1:]
-    return (version[0], version, int(version.replace(".", "")))
+    return int(version.split(".")[0]), version, tuple(map(int, version.split(".")))
 
 
 def get_grafana_releases():
     r = requests.get(
-        "https://api.github.com/repos/grafana/grafana/releases?per_page=50",
+        "https://api.github.com/repos/grafana/grafana/releases?per_page=100",
         headers={"Accept": "application/vnd.github.v3+json"},
     )
     if r.status_code != 200:
@@ -20,19 +20,18 @@ def get_grafana_releases():
     return r.json()
 
 
-by_major = {}
-
 if __name__ == "__main__":
     releases = get_grafana_releases()
-    for item in releases:
-        if item.get("prerelease"):
-            continue
-        major, version, as_int = get_by_major(item.get("tag_name"))
-        if major not in by_major.keys() or by_major[major]["as_int"] < as_int:
-            by_major[major] = {"version": version, "as_int": as_int}
-    latest_3_majors = sorted(list(by_major.keys()), reverse=True)[:3]
+    by_major = {}
 
-    latest_releases = []
-    for idx in latest_3_majors:
-        latest_releases.append(by_major[idx]["version"])
+    for release in releases:
+        if release.get("prerelease"):
+            continue
+        major, version, as_tuple = get_by_major(release.get("tag_name"))
+        if major not in by_major.keys() or by_major[major]["as_tuple"] < as_tuple:
+            by_major[major] = {"version": version, "as_tuple": as_tuple}
+
+    latest_3_majors = sorted(list(by_major.keys()))[:3]
+    latest_releases = [by_major[idx]["version"] for idx in latest_3_majors]
+
     print(json.dumps(latest_releases))
