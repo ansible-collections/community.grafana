e360e3aaaf47782a260aede5cda53ef0df2f8318
Nemental <15136847+Nemental@users.noreply.github.com>
2024-05-31 11:29:00 +0200

    fix: remove deprecation check for api_key version

--- plugins/lookup/grafana_dashboard.py
+++ plugins/lookup/grafana_dashboard.py
@@ -152,16 +152,10 @@ class GrafanaAPI:
 
     def grafana_headers(self):
         headers = {"content-type": "application/json; charset=utf8"}
-        if self.grafana_api_key:
-            api_key = self.grafana_api_key
-            if len(api_key) % 4 == 2:
-                display.deprecated(
-                    "Passing a mangled version of the API key to the grafana_dashboard lookup is no longer necessary and should not be done.",
-                    "2.0.0",
-                    collection_name="community.grafana",
-                )
-                api_key += "=="
-            headers["Authorization"] = "Bearer %s" % api_key
+        if module.params.get("grafana_api_key", None):
+            self.headers["Authorization"] = (
+                "Bearer %s" % module.params["grafana_api_key"]
+            )
         else:
             headers["Authorization"] = basic_auth_header(
                 self.grafana_user, self.grafana_password
