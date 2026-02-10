f6292f7c4356f32b038d0e253784bfaf2a8f1efd
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2026-02-10 11:12:18 +0100

    Apply suggestion to not fail on non existing datasource

--- plugins/modules/grafana_datasource.py
+++ plugins/modules/grafana_datasource.py
@@ -950,20 +950,16 @@ def main():
         payload = get_datasource_payload(module.params, grafana_iface.org_id)
         if ds is None:
             ds_type = module.params["ds_type"]
-            enabled = grafana_iface.plugin_exists(ds_type)
-            if not enabled:
-                module.fail_json(
-                    failed=True, msg="Datasource type unknown: %s" % ds_type
-                )
-            else:
-                grafana_iface.create_datasource(payload)
+            if not grafana_iface.plugin_exists(ds_type):
+                module.log(msg=f"Datasource type unknown: {ds_type}")
+            grafana_iface.create_datasource(payload)
+            ds = grafana_iface.datasource_by_name(name)
+            if ds.get("isDefault") != module.params["is_default"]:
+                grafana_iface.update_datasource(ds.get("id"), payload)
                 ds = grafana_iface.datasource_by_name(name)
-                if ds.get("isDefault") != module.params["is_default"]:
-                    grafana_iface.update_datasource(ds.get("id"), payload)
-                    ds = grafana_iface.datasource_by_name(name)
-                module.exit_json(
-                    changed=True, datasource=ds, msg="Datasource %s created" % name
-                )
+            module.exit_json(
+                changed=True, datasource=ds, msg=f"Datasource {name} created"
+            )
         else:
             diff = compare_datasources(payload.copy(), ds.copy(), enforce_secure_data)
             if diff.get("before") == diff.get("after"):
